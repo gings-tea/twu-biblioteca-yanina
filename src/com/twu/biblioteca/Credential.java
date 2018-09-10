165b16b95c661ef12114399ae1c5fd01a48c0f99
Yanina <yanina.ovando@thoughtworks.com>
2018-09-10 12:17:34 -0300

    added credential.eauals

--- src/com/twu/biblioteca/Credential.java
+++ src/com/twu/biblioteca/Credential.java
@@ -8,4 +8,11 @@ public class Credential {
         this.libraryNumber = libraryNumber;
         this.password = password;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        Credential otherCredential = (Credential) obj;
+        return this.libraryNumber.equals(otherCredential.libraryNumber) &&
+                this.password.equals(otherCredential.password);
+    }
 }
