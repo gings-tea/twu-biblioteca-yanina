08d6d2c6d027be12338a656cd9dd3f59b40aa461
Yanina <yanina.ovando@thoughtworks.com>
2018-09-12 12:08:05 -0300

    Added log in

--- src/com/twu/biblioteca/UI.java
+++ src/com/twu/biblioteca/UI.java
@@ -1,23 +1,26 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.models.Book;
+import com.twu.biblioteca.models.Credential;
 
 import java.util.Scanner;
 
 class UI {
     private String welcomeMessage = "*** WELCOME TO THE TWU LIBRARY ***";
-    private String notLoggedOptions = "\n1) List all books\n2) List all movies\n3) Check out movie\n4) Log In\n5) Quit\nPlease select an option: ";
-    private String loggedOptions = "\n1) List all books\n2) Check out book\n3) Return book\n4) List all movies\n5) Check out movie\n6) Log Out\n7) Quit\nPlease select an option: ";
-    private String format = "%-10s  %-25s  %-5s \n";
+    private String commonOptions = "\n1) List all books\n2) Check out book\n3) Return book\n4) List all movies\n5) Check out movie\n6) ";
+    private String format = "%-25s  %-25s  %-5s \n";
     private String returnOkMsg = "Thank you for returning the book";
     private String returnErrorMsg = "That is not a valid book to return.";
     private String checkOutOkMsg = "Thank you! Enjoy the book";
     private String checkOutErrorMsg = "That book is not available.";
     private boolean isLogged;
-    BookManager bookManager;
+    private BookManager bookManager;
+    private UserManager userManager;
 
     UI() {
+
         bookManager = new BookManager();
+        userManager = new UserManager();
         isLogged = false;
     }
 
@@ -27,86 +30,48 @@ class UI {
 
     void optionManager(){
         Scanner sc = new Scanner(System.in);
-        boolean didNotQuit;
-        do{
-            if(isLogged){
-                didNotQuit = loggedCase(sc);
-            }
-            else{
-                didNotQuit = notLoggedCase(sc);
-            }
-        } while(didNotQuit);
-    }
-
-    private boolean loggedCase(Scanner sc){
-        int optionSelected;
-        boolean didNotQuit = true;
+        String optionSelected;
         Book book;
-        System.out.print(loggedOptions);
-        optionSelected = Integer.valueOf(sc.nextLine());
-        switch(optionSelected){
-            case 1:
-                listAllBooks();
-                break;
-            case 2:
-                book = enterLibraryBookInformation(sc);
-                modifyAvailabilityOfBook(book, false, checkOutOkMsg, checkOutErrorMsg);
-                break;
-            case 3:
-                book = enterLibraryBookInformation(sc);
-                modifyAvailabilityOfBook(book, true, returnOkMsg, returnErrorMsg);
-                break;
-            case 7:
-                didNotQuit = false;
-                break;
-            default:
-                System.out.println();
-                System.out.println("Select a valid option!");
-                System.out.println();
-        }
-        return didNotQuit;
+        do{
+            optionPrint();
+            optionSelected = sc.nextLine();
+            switch(optionSelected){
+                case "1":
+                    listAllBooks();
+                    break;
+                case "2":
+                    book = enterLibraryBookInformation(sc);
+                    modifyAvailabilityOfBook(book, false, checkOutOkMsg, checkOutErrorMsg);
+                    break;
+                case "3":
+                    book = enterLibraryBookInformation(sc);
+                    modifyAvailabilityOfBook(book, true, returnOkMsg, returnErrorMsg);
+                    break;
+
+                case "6":
+                    isLogged = logIn(sc);
+                case "q":
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Select a valid option!");
+                    System.out.println();
+            }
+        } while(isNotQuitting(optionSelected));
     }
 
-    private boolean notLoggedCase(Scanner sc) {
-        int optionSelected;
-        boolean didNotQuit = true;
-        System.out.print(notLoggedOptions);
-        optionSelected = Integer.valueOf(sc.nextLine());
-        switch(optionSelected){
-            case 1:
-                listAllBooks();
-                break;
-            case 2:
-                System.out.println("not implemented");
-                break;
-            case 3:
-                System.out.println("not implemented");
-                break;
-            case 4:
-                System.out.println("not implemented");
-                break;
-            case 5:
-                didNotQuit = false;
-                break;
-            default:
-                System.out.println();
-                System.out.println("Select a valid option!");
-                System.out.println();
-        }
-        return didNotQuit;
-    }
-
-
-    private void modifyAvailabilityOfBook(Book book, boolean availability, String successMsg, String errorMsg) {
+    private boolean logIn(Scanner sc){
         System.out.println();
-        // TODO: Cambiar el usuario
-        if (bookManager.changeAvailability(book, availability,"111-1111")){
-            System.out.println(successMsg);
-        } else {
-            System.out.println(errorMsg);
-        }
+        System.out.print("Please enter your library ID: ");
+        String libraryID = sc.nextLine();
+        System.out.print("Please enter your password: ");
+        char[] password = sc.nextLine().toCharArray();
+        return userManager.loginUser(new Credential(libraryID, password));
     }
 
+    private boolean isNotQuitting(String optionSelected) {
+        return !optionSelected.toLowerCase().equals("q");
+    }
 
     private Book enterLibraryBookInformation(Scanner sc){
         System.out.println();
@@ -119,10 +84,30 @@ class UI {
         return new Book(title, author, yearPublished);
     }
 
+    private void optionPrint(){
+        if(isLogged){
+            System.out.print(commonOptions + "Log out\nPlease enter an option or enter Q to quit: ");
+        }
+        else {
+            System.out.print(commonOptions + "Log In\nPlease enter an option or enter Q to quit: ");
+        }
+    }
+
     private void listAllBooks(){
         System.out.println();
         System.out.println("Available books");
         System.out.printf(format,"Title","Author","Year Published");
         bookManager.getAvailableBookDetails(format);
     }
+
+    private void modifyAvailabilityOfBook(Book book, boolean availability, String successMsg, String errorMsg) {
+        System.out.println();
+        // TODO: Cambiar el usuario
+        if (bookManager.changeAvailability(book, availability,"111-1111")){
+            System.out.println(successMsg);
+        } else {
+            System.out.println(errorMsg);
+        }
+    }
+
 }
--- src/com/twu/biblioteca/UserManager.java
+++ src/com/twu/biblioteca/UserManager.java
@@ -8,6 +8,10 @@ import java.util.ArrayList;
 public class UserManager {
     private ArrayList<User> registerUsers;
 
+    public UserManager() {
+        fillUsers();
+    }
+
     public UserManager(ArrayList<User> registerUsers) {
         this.registerUsers = registerUsers;
     }
@@ -29,5 +33,11 @@ public class UserManager {
             }
         }
         return new String[0];
+    }
+
+    private void fillUsers(){
+        registerUsers = new ArrayList<>();
+        registerUsers.add(new User("Yanina", "a@a.com", "+1234", new Credential("123-4567","password".toCharArray())));
+        registerUsers.add(new User("Collen", "email@a.com", "+9876", new Credential("987-6543","pass".toCharArray())));
     }
 }
